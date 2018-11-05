0a01b722ad326c9d0594202e91ec9af35c3fcf49
daxcpp <cdavide98carnemolla@gmail.com>
2018-11-05 22:13:47 +0100

    Add /start command.

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from evaluate import *
 
 # Telegram
 import telegram
@@ -12,3 +11,11 @@ tokenconf = tokenconf.replace("\n", "")
 
 # Token of your telegram bot that you created from @BotFather, write it on token.conf
 TOKEN = tokenconf
+
+
+#Bot functions
+
+#Function: start_cmd
+#Send message with bot's information
+def start_cmd(bot, update):
+    bot.sendMessage(chat_id = update.message.chat_id, text="Informazioni bot")
--- main.py
+++ main.py
@@ -7,14 +7,13 @@ bot = telegram.Bot(TOKEN)
 #Function: main
 #Main function that run the bot.
 def main():
-	updater = Updater(TOKEN)
-	dp = updater.dispatcher
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
 
-	#Bot Command
-    
+    dp.add_handler(CommandHandler('start', start_cmd))
 
-	updater.start_polling()
-	updater.idle()
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     main()
