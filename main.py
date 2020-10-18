df8ee2c61da28792773a8ec3da58379a7c69c999
TendTo <casablancaernesto@gmail.com>
2020-10-18 16:46:09 +0200

    change: the bot will now support channel comments

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
     dp.add_handler(CommandHandler('rules', rules_cmd))
     dp.add_handler(CommandHandler('ban', ban_cmd, pass_args = True))
     dp.add_handler(MessageHandler(Filters.reply , message_handle))
+    dp.add_handler(MessageHandler(Filters.forwarded, comment_msg))
     dp.add_handler(CallbackQueryHandler(callback_spot))
 
 
