3527bd3b5ca10b9ac964c1d16765d2c97d9b1bb3
TendTo <casablancaernesto@gmail.com>
2021-08-30 20:22:48 +0200

    add: utility init scripts

---
+++ script/init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Root folder of the project
+SCRIPT=$(readlink -f "$0")
+LOC=$(dirname "$SCRIPT")/..
+
+# Create virtual environment
+python3 -m venv $LOC/venv
+
+# Activate virtual environment
+source $LOC/venv/bin/activate
+
+# Update pip
+pip install --upgrade pip
+
+# Install requirements and dev requirements
+pip3 install -r $LOC/requirements.txt
+pip3 install -r $LOC/requirements_dev.txt
+
+echo "$(pip3 --version)"
