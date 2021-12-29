dd2a24f0a0995728ffe3b0996ce9c563c71d4bbf
TendTo <casablancaernesto@gmail.com>
2021-12-29 13:16:02 +0100

    fix: makes sure the current branch is upgrade before resetting

--- script/reset-origin.bat
+++ script/reset-origin.bat
@@ -3,5 +3,6 @@ rem Hard resets the current branch so that it is even with the https://github.co
 rem The updated branch is then force pushed to the remote origin, which should be your fork.
 rem WARNING: you will lose all local commits that are not on the https://github.com/UNICT-DMI/Telegram-SpottedDMI-Bot.git repo
 git fetch https://github.com/UNICT-DMI/Telegram-SpottedDMI-Bot.git refs/heads/upgrade:dmi-upstream/upgrade
+git switch upgrade
 git reset --hard dmi-upstream/upgrade
 git push -f origin upgrade
