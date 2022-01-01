90ffc29b07a3cbb42e8dac35368b0a42ea9ab3d5
drendog <graziano27dg@gmail.com>
2022-01-01 20:00:40 +0100

    feat: add anon credits on anon comments

--- modules/handlers/anonym_comment.py
+++ modules/handlers/anonym_comment.py
@@ -1,7 +1,9 @@
 """Anonym Comment on a post in the comment group"""
+from random import choice
 from telegram import Update
 from telegram.ext import CallbackContext
 from modules.data import config_map
+from modules.data.data_reader import read_md
 from modules.utils import EventInfo
 
 
@@ -16,5 +18,7 @@ def anonymous_comment_msg(update: Update, context: CallbackContext):
     info = EventInfo.from_message(update, context)
 
     if info.chat_id == config_map['meme']['channel_group_id'] and info.message.via_bot is None:
-        info.message.copy(chat_id=info.chat_id, reply_to_message_id=info.message.reply_to_message.message_id)
+        info.bot.send_message(chat_id=info.chat_id, 
+                                text=info.message.text + '\n\nby: ' + choice(read_md("anonym_names").split("\n")),
+                                reply_to_message_id=info.message.reply_to_message.message_id)
         info.message.delete()
