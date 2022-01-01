a32f002e6cfc2f3c75f4dbf9d881890e795f9cd7
drendog <graziano27dg@gmail.com>
2022-01-01 20:23:11 +0100

    fix: anon comments on supergroup not processed fix

--- modules/handlers/anonym_comment.py
+++ modules/handlers/anonym_comment.py
@@ -20,5 +20,5 @@ def anonymous_comment_msg(update: Update, context: CallbackContext):
     if info.chat_id == config_map['meme']['channel_group_id'] and info.message.via_bot is None:
         info.bot.send_message(chat_id=info.chat_id, 
                                 text=info.message.text + '\n\nby: ' + choice(read_md("anonym_names").split("\n")),
-                                reply_to_message_id=info.message.reply_to_message.message_id)
+                                reply_to_message_id=info.message.reply_to_message.message_id if info.message.reply_to_message else None)
         info.message.delete()
