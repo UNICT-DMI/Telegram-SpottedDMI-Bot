1941235a7d77224a3238f018523adb7d7288bf79
Tend <65033249+TendTo@users.noreply.github.com>
2022-03-16 21:33:48 +0100

    fix: add delete_anonymous_comments setting (#75)

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def add_handlers(dp: Dispatcher):
     if config_map['meme']['comments']:
         dp.add_handler(
             MessageHandler(Filters.forwarded & Filters.chat_type.groups & Filters.is_automatic_forward, forwarded_post_msg))
-    if config_map['meme']['replace_anonymous_comments']:
+    if config_map['meme']['delete_anonymous_comments']:
         dp.add_handler(
             MessageHandler(Filters.sender_chat.channel & Filters.chat_type.groups & ~Filters.is_automatic_forward,
                            anonymous_comment_msg,
