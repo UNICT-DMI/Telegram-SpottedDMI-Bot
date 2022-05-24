a4a00f2d70307bca7d0807f2c31ce06329715a84
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-05-24 19:01:13 +0200

    tests: use of class inside test_stats.py

--- tests/unit/test_stats.py
+++ tests/unit/test_stats.py
@@ -1,21 +1,26 @@
+"""Test all the modules related to the stats functionalities"""
+from pytest_mock import MockerFixture
 from modules.data import PostData
 from modules.handlers.stats import REACTION, avg_callback
 
-from pytest_mock import MockerFixture
 
-def test_avg_callback(mocker: MockerFixture):
-    # arrange
-    mock_return = 1
-    mock_param = "5"
-    mocker.patch.object(PostData, "get_avg", return_value=mock_return)
-    spy = mocker.spy(PostData, "get_avg")
+class TestStats:
+    """Test the stats handlers"""
 
-    # act
-    res1 = avg_callback("votes")
-    res2 = avg_callback(mock_param)
+    def test_avg_callback(self, mocker: MockerFixture):
+        """Tests the avg_callback handler"""
+        # arrange
+        mock_return = 1
+        mock_param = "5"
+        mocker.patch.object(PostData, "get_avg", return_value=mock_return)
+        spy = mocker.spy(PostData, "get_avg")
 
-    # assert
-    assert res1 == (f"Gli spot ricevono in media {mock_return} voti")
-    assert res2 == (f"Gli spot ricevono in media {mock_return} {REACTION[mock_param]}")
-    assert spy.call_count == 2
-    assert spy.spy_return == mock_return
+        # act
+        res1 = avg_callback("votes")
+        res2 = avg_callback(mock_param)
+
+        # assert
+        assert res1 == (f"Gli spot ricevono in media {mock_return} voti")
+        assert res2 == (f"Gli spot ricevono in media {mock_return} {REACTION[mock_param]}")
+        assert spy.call_count == 2
+        assert spy.spy_return == mock_return
