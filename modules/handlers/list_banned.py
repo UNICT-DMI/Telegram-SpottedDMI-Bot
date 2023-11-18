7aff629910282a163c052b0d027d31768f108967
alepiaz <alessiopiazza117@gmail.com>
2023-11-18 22:29:04 +0000

    wrote docstrings and removed get_human_readable_date

--- modules/handlers/list_banned.py
+++ modules/handlers/list_banned.py
@@ -6,17 +6,31 @@ from modules.data import DbManager
 from modules.utils import EventInfo
 
 def list_banned_cmd(update: Update, context: CallbackContext):
+    """The command to get a list of the current banned users
+    with the user_id and date and time of the ban   
+
+    Args:
+        update (Update): update event
+        context (CallbackContext): context passed by the handler
+    """
     info = EventInfo.from_message(update, context)
     users = DbManager.select_from(select="*", table_name="banned_users")
     info.bot.send_message(chat_id=info.chat_id, text=f"{write_banned_list(users)}",parse_mode='Markdown')
 
 def write_banned_list(users: list)->str:
-    text = "*La lista dei bannati:*\n\n"
+    """Write the list of banned users
+
+    Args:
+        users (list): a list of dictionaries containing query results
+
+    Returns:
+        str: the text of the message
+    """
+    text = "*La lista dei bannati:*\n"
     for user in users:
-        date = get_human_readable_datetime(user["ban_date"])
+        datetime_object = datetime.strptime(user['ban_date'], '%Y-%m-%d %H:%M:%S')
+        date = datetime_object.strftime('%d/%m/%Y %H:%M')
         text += f"• {user['user_id']} bannato il {date}\n"
+        
     return text
 
-def get_human_readable_datetime(date:str)->str:
-    datetime_object = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-    return datetime_object.strftime('%d/%m/%Y %H:%M')
