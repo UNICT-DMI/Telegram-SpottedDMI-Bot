d12334602df681d3134ab5b2b565e0d6970e159b
Stefano Borzì <stefanoborzi32@gmail.com>
2023-11-29 10:46:28 +0100

    chore: clean old code, remove useless reference and scripts (#143)
    * chore: clean old code, remove useless reference and scripts
    
    * chore: restore local-ci scripts

--- script/reset-origin.sh
+++
@@ -1,7 +0,0 @@
-# Hard resets the current branch so that it is even with the https://github.com/UNICT-DMI/Telegram-SpottedDMI-Bot.git repo
-# The updated branch is then force pushed to the remote origin, which should be your fork.
-# WARNING: you will lose all local commits that are not on the https://github.com/UNICT-DMI/Telegram-SpottedDMI-Bot.git repo
-git fetch https://github.com/UNICT-DMI/Telegram-SpottedDMI-Bot.git refs/heads/upgrade:dmi-upstream/upgrade
-git switch upgrade
-git reset --hard dmi-upstream/upgrade
-git push -f origin upgrade
