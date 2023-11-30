7380b9b07d0fc44fc7974af54a890f21d22e981b
alepiaz <alessiopiazza117@gmail.com>
2023-11-30 22:58:33 +0000

    feat: added a custom filter to check if sender is admin

---
+++ src/spotted/handlers/custom_filters.py
@@ -0,0 +1,18 @@
+"""Defines custom filters to use for commands"""
+from telegram import Message, Chat
+from telegram.ext.filters import MessageFilter
+
+
+class IsAdminFilter(MessageFilter):
+    """Check if the message from the update was sent by
+        one of the administrators of the group
+    Args:
+        MessageFilter: the superclass for the filter
+    """
+
+    def filter(self, message: Message):
+        chat = message.chat
+        sender_id = message.from_user.id
+        if chat.type in [Chat.SUPERGROUP, Chat.GROUP]:
+            return sender_id in [admin.id for admin in chat.get_administrators(chat.id)]
+        return False
