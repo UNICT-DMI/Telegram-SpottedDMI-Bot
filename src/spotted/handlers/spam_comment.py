a17fced4e4bc5e442e0cd08c1df7019445d848cf
Helias <stefanoborzi32@gmail.com>
2024-02-25 12:58:32 +0100

    Revert "chore: fix test and lint packages versions"
    This reverts commit c47e0855f0def2a54b797acbf84bdf7e89ee8265.

--- src/spotted/handlers/spam_comment.py
+++ src/spotted/handlers/spam_comment.py
@@ -7,7 +7,7 @@ from spotted.data import Config
 from spotted.utils import EventInfo
 
 
-async def spam_comment_msg(update: Update, context: CallbackContext) -> None:
+async def spam_comment_msg(update: Update, context: CallbackContext):
     """Handles a spam comment on a post in the comment group.
     Deletes the original post.
 
